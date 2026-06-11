2372b0ec2b064dfef3ab2ca8c5ca0157fb6f9361
ariandreoli <arii.andreooli@gmail.com>
2026-06-11 08:29:33 -0300

    ejercicio15

---
+++ e15.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import numpy as np
+
+imag = Image.open("imagen.jpg")
+
+imagris = imag.convert("L")
+
+m = np.array(imagris)
+
+imagris.show()
+
+filas = len(m)
+columnas = len(m[0])
+
+volteada = m.copy()
+
+for f in range(filas):
+    for i in range(columnas):
+        j = columnas - 1 - i
+        volteada[f][j] = m[f][i]
+
+imavolteada = Image.array(volteada)
+
+imavolteada.show()
+
